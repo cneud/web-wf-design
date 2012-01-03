6ed1da057d12e451770a90756cc59d3bc1f686fc
alex@mygrid.org.uk <alex@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-01-03 13:28:52 +0000

    Added deleting wirings from filesystem.

--- web-wf-design/src/java/net/sf/taverna/portal/wireit/DeleteWireit.java
+++ web-wf-design/src/java/net/sf/taverna/portal/wireit/DeleteWireit.java
@@ -1,5 +1,6 @@
 package net.sf.taverna.portal.wireit;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -7,6 +8,7 @@ import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.FileUtils;
 
 /**
  * Deletes a Wiring from the SQL database.
@@ -14,7 +16,14 @@ import javax.servlet.http.HttpServletResponse;
  * @author Christian
  */
 public class DeleteWireit extends WireitSQLBase {
-    
+
+    public static final String JSON_WIRINGS_DIR_PARAMETER = "JSON_WIRINGS_DIR";
+    public static final String SAVE_TO_PARAMETER = "SAVE_TO";
+    public static final String SAVE_TO_FILESYSTEM = "filesystem";
+    public static final String SAVE_TO_DATABASE = "database";
+    private File jsonWiringsDir = null; // directory where we save JSON wirings as individual files
+    private String SAVE_TO; // wirings will be saved to/loaded from filesystem or database based on the value of this context parameter
+
     /**
      * Sets up the servlet and creates an SQL statement against which queries can be run.
      * 
@@ -25,8 +34,29 @@ public class DeleteWireit extends WireitSQLBase {
         super();
     }
 
+    @Override
+    public void init(){
+        String jsonWiringsDirPath = getServletContext().getInitParameter(JSON_WIRINGS_DIR_PARAMETER);
+        SAVE_TO = getServletContext().getInitParameter(SAVE_TO_PARAMETER);
+        if (SAVE_TO == null){
+            throw new RuntimeException("Method of saving JSON wirings (filesystem or database) has not been configured in web.xml. Check context parameter " + SAVE_TO_PARAMETER +".");
+        }
+        else{
+            if (SAVE_TO.toLowerCase().equals(SAVE_TO_FILESYSTEM)) {
+                if (jsonWiringsDirPath != null) {
+                    jsonWiringsDir = new File(jsonWiringsDirPath);
+                    if (!jsonWiringsDir.exists()) {
+                        jsonWiringsDir.mkdirs();
+                    }
+                } else {
+                    throw new RuntimeException("Directory where to save to/load from JSON wirings has not been configured in web.xml. Check context parameter " + JSON_WIRINGS_DIR_PARAMETER + ".");
+                }
+            }
+        }
+    }
+    
     /**
-     * Deletes a Wiring from the SQL database.
+     * Deletes a Wiring.
      * 
      * If a wiring exists in the database with a name and language that matches the part of the URL after the ?
      * It will be deleted.
@@ -47,10 +77,14 @@ public class DeleteWireit extends WireitSQLBase {
         System.out.println((new Date()) + "in DeleteWireit.doGet");
              
         String name = request.getParameter("name");
-        String encodeName = URLEncoder.encode(name);
+        String encodedName = URLEncoder.encode(name);
         String language = request.getParameter("language");
-        try {
-            deleteWorking(encodeName, language);
+       try {
+            if (SAVE_TO.toLowerCase().equals(SAVE_TO_FILESYSTEM)) {
+                deleteJSONWiringFromFileSystem(encodedName); // delete from the filesystem
+            } else {
+                deleteJSONWiringFromDatabase(encodedName, language); // delete from the database
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
             throw new ServletException(ex);
@@ -74,12 +108,27 @@ public class DeleteWireit extends WireitSQLBase {
      * @throws SQLException Thrown if SQL update fails.
      * 
      */
-    private void deleteWorking(String name, String language) throws SQLException {
+    private void deleteJSONWiringFromDatabase(String name, String language) throws SQLException {
         String sqlQuery;
         sqlQuery = "delete from wirings where name = \"" + name + "\" and language = \"" + language + "\"";            
         System.out.println("Running: " +  sqlQuery);
         int count = executeUpdate(sqlQuery);
         System.out.println(count + " wirings deleted");
+    }
+
+    private void deleteJSONWiringFromFileSystem(String encodedName) {
+
+        File jsonFile = new File(jsonWiringsDir, encodedName + ".json");
+        File jsonBackupFile = new File(jsonWiringsDir, encodedName + ".json.bkp");
+        System.out.println("Deleting JSON wiring " + encodedName + " from " + jsonFile.getAbsolutePath());
+        
+        if (jsonFile.exists()){
+            FileUtils.deleteQuietly(jsonFile);
+        }
+        if (jsonBackupFile.exists()){
+            FileUtils.deleteQuietly(jsonBackupFile);
+        }
+
     }
     
 }
